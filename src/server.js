d4b473b9f8d999ad03fa0ecb694a9e98b5f7823f
Thanh Nguyen <nguyenlongthanhIT@gmail.com>
2022-10-31 21:19:43 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(session({ secret: "secret" }));
 
 configViewEngine(app);
 initWebRoute(app);
-initAPIRoute(app);
+// initAPIRoute(app);
 // app.use(notFound);
 // app.use(errorHandlerMiddleware);
 
