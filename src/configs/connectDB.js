dc8468f4c64ed8e1755f6e7da0814cf0fe11c766
Thanh Nguyen <nguyenlongthanhIT@gmail.com>
2022-11-02 10:42:30 +0700

    first commit

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,9 +1,17 @@
 const mysql = require("mysql2/promise");
 
+// const pool = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     database: "ThanhDatMobile",
+// });
+
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "ThanhDatMobile",
+    host: "sql12.freesqldatabase.com",
+    database: "sql12534782",
+    user: "sql12534782",
+    password: "z6KAel6pm8",
+    port: "3306",
 });
 
 module.exports = pool;
