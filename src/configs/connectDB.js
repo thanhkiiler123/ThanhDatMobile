1a3eaa9600c4c53d1826a75af53bfac35aba37ed
thanh <thanh@thanh.thanhnguyen.com>
2023-05-25 16:29:04 +0700

    update

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     host: "buxpviafyttqy6i8ye78-mysql.services.clever-cloud.com",
     user: "uucgkid0kbwed9rj",
     database: "buxpviafyttqy6i8ye78",
-    password: "uucgkid0kbwed9rj",
+    password: "avPYf1yykq6q8MNg3IhW",
     port: "3306",
 });
 
